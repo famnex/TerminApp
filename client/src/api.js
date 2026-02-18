0720d5f80ccf78bb50915881ce3dc6030ab7aeae
famnex <famnex@users.noreply.github.com>
2026-02-18 13:04:45 +0100

    fix: update baseURL to absolute API path for login

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'api',
+    baseURL: '/launcher/termin_new/server/public/api',
     headers: {
         'Content-Type': 'application/json',
     },
