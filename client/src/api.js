fdbf6953a6dde745d8faff42c44fcea8bf01a059
famnex <famnex@users.noreply.github.com>
2026-03-18 12:15:07 +0100

    fix: resolve production api url path

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.DEV ? '/api' : '/launcher/termin_new/server/public/api',
     headers: {
         'Content-Type': 'application/json',
     },
